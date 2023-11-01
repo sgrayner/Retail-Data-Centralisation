d54896342d64d478d9201e207125c35c267817b2
sgrayner <srayner393@gmail.com>
2023-11-01 19:56:46 +0000

    Committing milestone_3.sql and milestone_4.sql

--- milestone_3.sql
+++ milestone_3.sql
@@ -21,6 +21,7 @@ ALTER TABLE dim_store_details
 	ALTER COLUMN longitude TYPE FLOAT USING longitude::double precision,
 	ALTER COLUMN locality TYPE VARCHAR(255),
 	ALTER COLUMN store_code TYPE VARCHAR(12),
+	ALTER COLUMN staff_numbers TYPE SMALLINT USING staff_numbers::smallint,
 	ALTER COLUMN opening_date TYPE DATE,
 	ALTER COLUMN store_type TYPE VARCHAR(225),
 	ALTER COLUMN latitude TYPE FLOAT USING latitude::double precision,
